c476cd31221d2b54476ef0ced6efdd16e49dc56a
RobRis75830 <45773650+RobRis75830@users.noreply.github.com>
2019-03-25 20:46:01 -0400

    Add files via upload

---
+++ src/main/java/navy/toonavy4u/Toonavy4uApplication.java
@@ -0,0 +1,13 @@
+package navy.toonavy4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toonavy4uApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Toonavy4uApplication.class, args);
+    }
+
+}
---
+++ src/test/java/navy/toonavy4u/Toonavy4uApplicationTests.java
@@ -0,0 +1,16 @@
+package navy.toonavy4u;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Toonavy4uApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
