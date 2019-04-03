3c54262fdc925dbb5346f0844525820cde51d7aa
Robert <rriselvato@cs.stonybrook.edu>
2019-04-02 21:25:47 -0400

    Removed "../static/" in the HTML references. Added placeholder controller methods for the HTML pages.

--- src/main/java/navy/toonavy4u/HomeController.java
+++ src/main/java/navy/toonavy4u/HomeController.java
@@ -11,9 +11,34 @@ import org.springframework.web.bind.annotation.*;
 @EnableAutoConfiguration
 public class HomeController {
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String newPost(Model model) {
         model.addAttribute("post", new Post());
         return "index";
     }
+
+    @RequestMapping(value = "Front.html", method = RequestMethod.GET)
+    public String front(Model model) {
+        model.addAttribute("post", new Post());
+        return "Front";
+    }
+
+    @RequestMapping(value = "Create.html", method = RequestMethod.GET)
+    public String create(Model model) {
+        model.addAttribute("post", new Post());
+        return "Create";
+    }
+
+    @RequestMapping(value = "Profile.html", method = RequestMethod.GET)
+    public String profile(Model model) {
+        model.addAttribute("post", new Post());
+        return "Profile";
+    }
+
+    @RequestMapping(value = "View.html", method = RequestMethod.GET)
+    public String view(Model model) {
+        model.addAttribute("post", new Post());
+        return "View";
+    }
+
 }
