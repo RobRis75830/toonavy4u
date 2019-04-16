e624c19545a9b256b3f4e587fc400f795a583935
Haejin <jenijung1771@gmail.com>
2019-04-16 00:16:48 -0700

    adding basics for pagelist. Needs to add more(Only series name and image file keys are added)

---
+++ src/main/java/keys/PagelistKey.java
@@ -0,0 +1,26 @@
+package keys;
+
+public class PagelistKey {
+    private String SeriesName; //series name
+    private String prodcutURL; //cover image
+
+    public String getSeriesName() {
+        return SeriesName;
+    }
+
+    public void setSeriesName(String seriesName) {
+        SeriesName = seriesName;
+    }
+
+    public String getProdcutURL() {
+        return prodcutURL;
+    }
+
+    public void setProdcutURL(String prodcutURL) {
+        this.prodcutURL = prodcutURL;
+    }
+
+    public PagelistKey(){
+
+    }
+}
---
+++ src/main/java/repositories/PageRepository.java
@@ -0,0 +1,8 @@
+package repositories;
+
+import java.util.List;
+import keys.PagelistKey;
+
+public interface PageRepository {
+    List<PagelistKey> listProduct();
+}
