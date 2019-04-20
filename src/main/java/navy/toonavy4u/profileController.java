3c1e2746e46163510d5a2a27739e3be602f7941b
xinyueZhao2019 <https://github.com/jenijung1771/NAVY_HW1.git>
2019-04-20 16:19:23 -0400

    my profile work2

---
+++ src/main/java/navy/toonavy4u/profileController.java
@@ -0,0 +1,117 @@
+package navy.toonavy4u;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import entities.Categories;
+import entities.Comic;
+import entities.Pages;
+import entities.Series;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import repositories.CategoriesRepository;
+import repositories.ComicRepository;
+import repositories.PagesRepository;
+import repositories.SeriesRepository;
+
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static navy.toonavy4u.LoginController.getEmail;
+
+@Controller
+@EnableAutoConfiguration
+public class profileController {
+    @Autowired
+    private SeriesRepository seriesRepository;
+
+    @Autowired
+    private CategoriesRepository categoriesRepository;
+    @Autowired
+    private ComicRepository comicRepository;
+    @Autowired
+    private PagesRepository pagesRepository;
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
+
+    @RequestMapping(value = "/myProfile", method = {RequestMethod.PUT,RequestMethod.GET})
+    public String search(Model model, OAuth2AuthenticationToken authentication) {
+        String email = "";
+
+        if (authentication != null) {
+            email = getEmail(authentication, authorizedClientService);
+        }
+        String userName="";
+        int before=0;
+        for (int i=0;i<email.length();i++){
+            if (email.charAt(i)== '@') {
+                i=email.length();
+            }else{
+                before+=1;
+            } }
+        userName=email.substring(0,before);
+        List<Series> creatSeries= seriesRepository.findByOwner(email);
+        Blob image;
+        byte[] bytes;
+        List<String> cover = new ArrayList<String>();
+        List<List> allcomic = new ArrayList<List>();
+        List<List> chapterCover = new ArrayList<List>();
+for (int i=0;i<creatSeries.size();i++){
+    image = creatSeries.get(i).getCover();
+        try {
+            int blobLength = (int) image.length();
+            bytes = image.getBytes(1, blobLength);
+            image.free();
+
+        } catch (SQLException ex) {
+            return "error";
+        }
+        String imageURL = "data:image/png;base64," + Base64.getEncoder().encodeToString(bytes);
+        cover.add(imageURL);
+        //finsh cover image
+    List<Comic> comic= comicRepository.findBySeriesOrderByCreatedAsc(creatSeries.get(i).getId());
+    allcomic.add(comic);
+    //finish add comic
+    List<String> coverChapter = new ArrayList<String>();
+    for (int k=0;k<comic.size();k++){
+        List<Pages> pages = pagesRepository.findByIdComicAndIdPageNumber(comic.get(k).getId(), 1);
+        Blob chimage=pages.get(0).getImage();
+        byte[] chbytes;
+        try {
+            int blobLength1 = (int) chimage.length();
+            chbytes = chimage.getBytes(1, blobLength1);
+            chimage.free();
+
+        } catch (SQLException ex) {
+            return "error";
+        }
+        String chiimageURL = "data:image/png;base64," + Base64.getEncoder().encodeToString(chbytes);
+        coverChapter.add(chiimageURL);
+
+    }
+    chapterCover.add(coverChapter);
+
+
+
+}
+
+
+
+        model.addAttribute("userName", userName);
+        model.addAttribute("creatSeries", creatSeries);
+        model.addAttribute("imageURL", cover);
+        model.addAttribute("comic", allcomic);
+        model.addAttribute("chapterCover", chapterCover);
+
+        return "Profile";
+    }
+
+}
