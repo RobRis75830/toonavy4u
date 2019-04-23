27175f6050103bbbf5f0e641462cc350696f5b60
Robert <rriselvato@cs.stonybrook.edu>
2019-04-22 22:08:05 -0400

    SecurityConfig : allows anonymous access to people's profile pages Profile : "Follow" button is disabled when not logged in

--- src/main/java/navy/toonavy4u/SecurityConfig.java
+++ src/main/java/navy/toonavy4u/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/css/**", "/fonts/**", "/images/**", "/js/**", "/ViewComic", "/ViewSeries", "/Toolbar.html", "/Browse", "/Random").permitAll()
+                .antMatchers("/", "/css/**", "/fonts/**", "/images/**", "/js/**", "/ViewComic", "/ViewSeries", "/Toolbar.html", "/Browse", "/Random", "/Profile").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
