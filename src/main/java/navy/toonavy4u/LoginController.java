87a91a3a6ddcfc547dbc2d9a6154c4252e3be4f8
Robert <rriselvato@cs.stonybrook.edu>
2019-04-30 17:50:10 -0400

    LoginController, Front : front page displays most popular series Create : cancels maps to "/"

--- src/main/java/navy/toonavy4u/LoginController.java
+++ src/main/java/navy/toonavy4u/LoginController.java
@@ -1,6 +1,9 @@
 package navy.toonavy4u;
 
+import entities.Comic;
+import entities.Rating;
 import entities.Reader;
+import entities.Series;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -14,10 +17,12 @@ import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import repositories.ReaderRepository;
+import repositories.*;
 
-import java.util.List;
-import java.util.Map;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Controller
 @SessionAttributes("email")
@@ -28,6 +33,18 @@ public class LoginController {
 
     @Autowired
     private ReaderRepository readerRepository;
+
+    @Autowired
+    private SeriesRepository seriesRepository;
+
+    @Autowired
+    private ComicRepository comicRepository;
+
+    @Autowired
+    private ViewsRepository viewsRepository;
+
+    @Autowired
+    private RatingRepository ratingRepository;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getLoginInfo(Model model, OAuth2AuthenticationToken authentication) {
@@ -48,6 +65,47 @@ public class LoginController {
             }
         }
 
+        List<Series> series = seriesRepository.findByPublished(1);
+        for (Series s : series) {
+            List<Comic> comics = comicRepository.findBySeriesOrderByCreatedAsc(s.getId());
+            List<Integer> comicIds = comics.stream().map(Comic::getId).collect(Collectors.toList());
+            long views = viewsRepository.countByIdComicIsIn(comicIds);
+            s.setViews(views);
+
+            ArrayList<Double> comicRatings = new ArrayList<>();
+            for (Comic c : comics) {
+                List<Rating> ratings = ratingRepository.findByIdComic(c.getId());
+                double rating = Math.round(ratings.stream().mapToDouble(Rating::getRating).average().orElse(0.0) * 100) / 100.0;
+                if (rating != 0.0) {
+                    comicRatings.add(rating);
+                }
+            }
+            double seriesRating = Math.round(comicRatings.stream().mapToDouble(value -> value).average().orElse(0.0) * 100) / 100.0;
+            s.setRating(seriesRating);
+        }
+        series.sort(Comparator.comparingDouble(s -> s.getViews() * (s.getRating() + 1)));
+        if (series.size() > 6) {
+            series = series.subList(series.size() - 6, series.size());
+        }
+        Collections.reverse(series);
+
+        ArrayList<String> imageURLs = new ArrayList<>();
+        for (Series s : series) {
+            Blob image = s.getCover();
+            byte[] bytes;
+            try {
+                int blobLength = (int) image.length();
+                bytes = image.getBytes(1, blobLength);
+                image.free();
+            } catch (SQLException ex) {
+                return "error";
+            }
+            String imageURL = "data:image/png;base64," + Base64.getEncoder().encodeToString(bytes);
+            imageURLs.add(imageURL);
+        }
+
+        model.addAttribute("popular", series);
+        model.addAttribute("imageURLs", imageURLs);
         model.addAttribute("post", new Post());
         return "Front";
     }
